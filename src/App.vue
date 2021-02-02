0d859ddd6b3992b0742dcc3c00be9d7d12be4a5e
Igor Semenov <semenow.igorek@yandex.ru>
2021-02-02 16:46:31 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Shop shop-title="Смартфоны" />
+    <shop shop-title="Ноутбуки" />
+  </div>
+</template>
+
+<script>
+  import Shop from "./components/Shop";
+
+  export default {
+    components: {
+      Shop
+    }
+  }
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
